f1f4302095c11a612d06ef06bbf5ad6bb51b1871
Jorge Nicho <jrgnichodevel@gmail.com>
2020-06-19 09:56:36 -0500

    tweaked wait for event period values

--- scxml_core/src/state_machine.cpp
+++ scxml_core/src/state_machine.cpp
@@ -44,8 +44,8 @@
 #include <log4cxx/patternlayout.h>
 #include <log4cxx/consoleappender.h>
 
-static const int WAIT_TRANSITION_PERIOD = 1000;                   // ms
-static const int WAIT_QT_EVENTS = WAIT_TRANSITION_PERIOD / 40.0;  // ms
+static const int WAIT_TRANSITION_PERIOD = 200;  // ms
+static const int WAIT_QT_EVENTS = 20;           // ms
 
 log4cxx::LoggerPtr createConsoleLogger(const std::string& logger_name)
 {
