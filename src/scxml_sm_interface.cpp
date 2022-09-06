4c7d2a82e4f7ba3eabac617b2798c248454bb934
Lily Baye-Wallace <72733784+lcbw@users.noreply.github.com>
2022-09-06 14:01:12 -0500

    adjusted getNeighbor's recursive component to search in a leaf-preference order  (#37)
    * adjusted getNeighbor's recursive component to search in a leaf-preference order
    
    * Minor updates
    
    Co-authored-by: Michael Ripperger <michael.ripperger@swri.org>

--- src/scxml_sm_interface.cpp
+++ src/scxml_sm_interface.cpp
@@ -165,11 +165,15 @@ QString ScxmlSMInterface::getNeighbor(const QString& state, const QString& trans
 
 QString ScxmlSMInterface::getActiveStateNeighbor(const QString& transition)
 {
-  for (const QString& state : sm_->activeStateNames(false))
+  const QStringList states = sm_->activeStateNames(false);
+
+  // `activeStateNames` returns the list of active state names, starting with the highest level state and ending with
+  // the leaf. Iterate over the active state names in reverse to start with the leaf state
+  for (auto it = states.rbegin(); it != states.rend(); ++it)
   {
     try
     {
-      return getNeighbor(state, transition);
+      return getNeighbor(*it, transition);
     }
     catch (const std::exception&)
     {
